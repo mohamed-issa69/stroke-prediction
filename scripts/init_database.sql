3a442aed130d69353d907ba5aa6d6d5c91b64b66
mohamed-issa69 <mohammedhamdi492004@gmail.com>
2026-03-19 18:05:15 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,24 @@
 
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'StrokePrediction')
+BEGIN
+    ALTER DATABASE StrokePrediction SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE StrokePrediction;
+END;
+GO
+
+
+CREATE DATABASE StrokePrediction;
+
+USE StrokePrediction;
+
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
